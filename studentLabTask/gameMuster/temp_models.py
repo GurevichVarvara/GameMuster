6735bd68e9b31101c3e0655d33f1721e6751c8bc
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-03-30 17:14:36 +0300

    Remove carousel, add pagination, add temporary game model

---
+++ studentLabTask/gameMuster/temp_models.py
@@ -0,0 +1,8 @@
+
+
+class Game:
+
+    def __init__(self, name, img_url, *genres):
+        self.name = name
+        self.img_url = img_url
+        self.genres = genres
--- studentLabTask/gameMuster/views.py
+++ studentLabTask/gameMuster/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from gameMuster.temp_models import Game
 
 
 def index(request):
-    return render(request, 'gameMuster/index.html')
+    game_list = [Game('PACKMAN',
+                      'https://i1.sndcdn.com/avatars-000527330727-10g55j-t240x240.jpg',
+                      'Arcade',
+                      'Oldschool')] * 10
+    game_paginator = Paginator(game_list, 4)
+
+    page_number = request.GET.get('page')
+    page_obj = game_paginator.get_page(page_number)
+
+    return render(request, 'gameMuster/index.html', {'game_list': game_list,
+                                                     'page_obj': page_obj})
 
 
 def detail(request):
