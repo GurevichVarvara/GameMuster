bc4380cf73b60e1838f3758e9d3d214374ce614c
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-04-01 14:19:44 +0300

    Add for-loop for platforms and genres in filter-panel

--- studentLabTask/gameMuster/views.py
+++ studentLabTask/gameMuster/views.py
@@ -11,7 +11,11 @@ def index(request):
     page_obj = game_paginator.get_page(page_number)
 
     return render(request, 'gameMuster/index.html', {'game_list': game_list,
-                                                     'page_obj': page_obj})
+                                                     'page_obj': page_obj,
+                                                     'platforms_available': ['PC', 'PS4'],
+                                                     'platforms_not_available': ['PS5'],
+                                                     'genres_available': ['Platformer'],
+                                                     'genres_not_available': ['Arcade']})
 
 
 def detail(request, game_id):
