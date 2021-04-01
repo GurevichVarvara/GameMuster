e88b9577fd975fab685c3078dbb3494f43c6732c
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-04-01 14:05:02 +0300

    Fix typo

--- studentLabTask/gameMuster/temp_models.py
+++ studentLabTask/gameMuster/temp_models.py
@@ -42,7 +42,7 @@ class ModelManager:
             datetime.now(),
             'somebody')] * 3
 
-        game = Game('PACKMAN',
+        game = Game('PACMAN',
                     'Pac-Man is a maze chase video game; the player controls the eponymous character through an '
                     'enclosed maze. The objective of the game is to eat all of the dots placed in the maze while '
                     'avoiding four colored ghosts — Blinky (red), Pinky (pink), Inky (cyan), and Clyde (orange) — '
