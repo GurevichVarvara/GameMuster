a6e7df9a392eea5647c692154fbad17f0fb915eb
varvarahurevich <gurevichvarvara@gmail.com>
2021-04-06 16:04:57 +0300

    Change the general function of receiving a response from the api to the function of receiving a response from the igdb api

--- studentLabTask/gameMuster/games_manager.py
+++ studentLabTask/gameMuster/games_manager.py
@@ -34,12 +34,12 @@ class GamesManager:
     def _get_access_twitter_token():
         return {'Authorization': 'Bearer {}'.format(TWITTER_BEARER_TOKEN)}
 
-    def _get_api_response(self, access_token_func, url, headers, params):
+    def _get_igdb_api_response(self, url, headers, params):
         response_from_api = requests.post(url, headers=headers, params=params)
 
         # if access token has expired
         if response_from_api.status_code != 200:
-            self.igdb_header = access_token_func()
+            self.igdb_header = self._get_access_igdb_token()
             response_from_api = requests.post(url, headers=headers, params=params)
 
         return response_from_api.json()
@@ -53,10 +53,9 @@ class GamesManager:
                   'filter[cover][not_eq]': 'null',
                   'filter[genres][not_eq]': 'null',
                   'filter[screenshots][not_eq]': 'null'}
-        result_games = self._get_api_response(self._get_access_igdb_token,
-                                              self.igdb_main_url + 'games/',
-                                              self.igdb_header,
-                                              params)
+        result_games = self._get_igdb_api_response(self.igdb_main_url + 'games/',
+                                                   self.igdb_header,
+                                                   params)
 
         games = []
         for result_game in result_games:
@@ -72,14 +71,12 @@ class GamesManager:
 
     def get_list_of_filters(self):
         params = {'fields': 'name'}
-        result_platforms = self._get_api_response(self._get_access_igdb_token,
-                                                  self.igdb_main_url + 'platforms/',
-                                                  self.igdb_header,
-                                                  params)
-        result_genres = self._get_api_response(self._get_access_igdb_token,
-                                               self.igdb_main_url + 'genres/',
-                                               self.igdb_header,
-                                               params)
+        result_platforms = self._get_igdb_api_response(self.igdb_main_url + 'platforms/',
+                                                       self.igdb_header,
+                                                       params)
+        result_genres = self._get_igdb_api_response(self.igdb_main_url + 'genres/',
+                                                    self.igdb_header,
+                                                    params)
 
         return ([platform['name'] for platform in result_platforms],
                 [genre['name'] for genre in result_genres])
@@ -96,7 +93,7 @@ class GamesManager:
         return [Tweet(result_tweet['full_text'],
                       datetime.datetime.strptime(result_tweet['created_at'],
                                                  '%a %b %d %H:%M:%S %z %Y'),
-                      result_tweet['user']['name']) \
+                      result_tweet['user']['name'])
                 for result_tweet in result_tweets]
 
     def get_description_of_game(self, game_id):
@@ -109,13 +106,14 @@ class GamesManager:
                             'screenshots.image_id,'
                             'platforms.name',
                   'filter[id][eq]': game_id}
-        result_game = self._get_api_response(self._get_access_igdb_token,
-                                             self.igdb_main_url + 'games/',
-                                             self.igdb_header,
-                                             params)[0]
+        result_game = self._get_igdb_api_response(self.igdb_main_url + 'games/',
+                                                  self.igdb_header,
+                                                  params)[0]
 
-        release_date = datetime.datetime.fromtimestamp(result_game['release_dates'][0]) \
-                        if 'release_dates' in result_game else 'no release date'
+        if 'release_dates' in result_game:
+            release_date = datetime.datetime.fromtimestamp(result_game['release_dates'][0])
+        else:
+            release_date = 'no release date'
 
         game.set_full_description(result_game['summary'],
                                   release_date,
@@ -123,19 +121,10 @@ class GamesManager:
                                   result_game.get('rating_count', 0),
                                   round(result_game.get('aggregated_rating', 0), 1),
                                   result_game.get('aggregated_rating_count', 0),
-                                  screenshots=[self._get_img(screenshot['image_id']) \
+                                  screenshots=[self._get_img(screenshot['image_id'])
                                                for screenshot in result_game['screenshots']],
-                                  platforms=[platform['name'] for platform \
+                                  platforms=[platform['name'] for platform
                                              in result_game['platforms']],
                                   tweets=self.get_tweets_by_game_name(game.name))
 
         return game
-
-
-
-
-
-
-
-
-
