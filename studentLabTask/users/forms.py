dcaf56b5ca18c5076d52c05a28bdb9d34896a27c
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-04-15 19:09:48 +0300

    Add editing profile

--- studentLabTask/users/forms.py
+++ studentLabTask/users/forms.py
@@ -1,12 +1,19 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, required=True, help_text='Required')
     first_name = forms.CharField(max_length=40, required=True, help_text='Required')
     last_name = forms.CharField(max_length=40, required=True, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+
+class UserEditForm(UserChangeForm):
 
     class Meta:
         model = User
--- studentLabTask/users/views.py
+++ studentLabTask/users/views.py
@@ -9,10 +9,39 @@ from django.urls import reverse_lazy
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.core.mail import send_mail
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from users.tokens import EmailConfirmationTokenGenerator
 from users.forms import SignupForm
+
+
+def send_confirmation_email(request, form):
+    if request.user.is_authenticated:
+        logout(request)
+
+    user = form.save(commit=False)
+
+    # Don't let user to login before email confirmation
+    user.is_active = False
+    user.save()
+
+    current_site = get_current_site(request)
+    send_mail(
+        subject='Email confirmation',
+        message=render_to_string('users/account_activation.html', {
+            'user': user,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': EmailConfirmationTokenGenerator().make_token(user),
+        }),
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[user.email],
+        fail_silently=False,
+    )
+
+    return render(request, 'users/base_message.html',
+                  {'message': 'Please confirm your email address '
+                              'to complete the registration'})
 
 
 class SignUpView(CreateView):
@@ -21,32 +50,30 @@ class SignUpView(CreateView):
     template_name = 'users/signup.html'
 
     def form_valid(self, form):
-        if self.request.user.is_authenticated:
-            logout(self.request)
+        return send_confirmation_email(self.request, form)
 
-        user = form.save(commit=False)
 
-        # Don't let user to login before email confirmation
-        user.is_active = False
-        user.save()
+def is_user_email_changed(prev_email, form):
+    current_email = form['email'].value()
 
-        current_site = get_current_site(self.request)
-        send_mail(
-            subject='Email confirmation',
-            message=render_to_string('users/account_activation.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': EmailConfirmationTokenGenerator().make_token(user),
-            }),
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[user.email],
-            fail_silently=False,
-        )
+    return current_email != prev_email
 
-        return render(self.request, 'users/base_message.html',
-                      {'message': 'Please confirm your email address '
-                                  'to complete the registration'})
+
+class UserEditView(UpdateView):
+    model = User
+    fields = ['username', 'first_name',
+              'last_name', 'email']
+    template_name = 'users/user_update_form.html'
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        if is_user_email_changed(self.get_object().email,
+                                 form):
+            response = send_confirmation_email(self.request, form)
+        else:
+            response = super(UserEditView, self).form_valid(form)
+
+        return response
 
 
 def activate(request, uidb64, token):
@@ -68,6 +95,3 @@ def activate(request, uidb64, token):
 
 def profile(request):
     return render(request, 'users/profile.html')
-
-
-
