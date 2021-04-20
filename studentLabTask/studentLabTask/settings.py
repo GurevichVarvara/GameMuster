43576e03e881746407a2c16066ff58a88bcf33e5
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-04-20 17:16:09 +0300

    Refactor games management

--- studentLabTask/studentLabTask/settings.py
+++ studentLabTask/studentLabTask/settings.py
@@ -47,6 +47,8 @@ AUTH_USER_MODEL = 'users.User'
 
 AUTHENTICATION_BACKENDS = ['users.backends.UserBackend']
 
+DEV_DATA_MODE = bool(os.environ.get('DEV_DATA_MODE', True))
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
