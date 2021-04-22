e5b97f8b8fdebc071d4c6e3ebde4ee386737fad7
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-04-22 13:33:00 +0300

    Add favorite game model

--- studentLabTask/gameMuster/models.py
+++ studentLabTask/gameMuster/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from safedelete.models import SafeDeleteModel, SOFT_DELETE
 
-# Create your models here.
+from users.models import User
+
+
+class FavoriteGame(SafeDeleteModel):
+    _safedelete_policy = SOFT_DELETE
+    game_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
