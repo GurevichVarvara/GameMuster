4bf4124469e9262fd1f6b6e1f3248bba0b80d404
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-04-22 16:49:10 +0300

    Close #29; Fix reset password pages

--- studentLabTask/users/urls.py
+++ studentLabTask/users/urls.py
@@ -1,23 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
-    PasswordResetCompleteView
 from users.views import SignUpView, activate, profile, UserEditView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('activate/<slug:uidb64>/<slug:token>/', activate, name='activate'),
     path('profile/', profile, name='profile'),
-    path('profile_edit/<int:pk>', UserEditView.as_view(), name='profile-edit'),
-    path('password_reset/',
-         PasswordResetView.as_view(template_name='password_reset/password_reset_form.html'),
-         name='password_reset'),
-    path('password_reset/done/',
-         PasswordResetDoneView.as_view(template_name='password_reset/password_reset_done.html'),
-         name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         PasswordResetConfirmView.as_view(template_name='password_reset/password_reset_confirm.html'),
-         name='password_reset_confirm'),
-    path('reset/done/',
-         PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_complete.html'),
-         name='password_reset_complete'),
+    path('profile_edit/<int:pk>', UserEditView.as_view(), name='profile-edit')
 ]
