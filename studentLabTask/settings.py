0d36283762e34f08f09f9f492c1dbb930f782719
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-04-23 16:30:53 +0300

    Add module to handle static files on heroku

--- studentLabTask/settings.py
+++ studentLabTask/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'gameMuster',
     'users',
     'safedelete',
+    'whitenoise',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
