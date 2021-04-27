8f8e7f79b9e9d23e023d7b83ac1f1112a2059c0d
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-04-27 18:47:56 +0300

    Create game and tweet models

--- gameMuster/models.py
+++ gameMuster/models.py
@@ -4,6 +4,27 @@ from safedelete.models import SafeDeleteModel, SOFT_DELETE
 from users.models import User
 
 
+class Game(models.Model):
+    game_id = models.IntegerField()
+    name = models.CharField(max_length=30)
+    release_date = model.DateTimeField()
+    must = model.BooleanField()
+    img_url = models.CharField(max_length=120)
+    description = models.TextField()
+    user_rating = models.DecimalField()
+    user_rating_count = models.IntegerField()
+    critics_rating = models.DecimalField()
+    critics_rating_count = models.IntegerField()
+    
+
+class Tweet(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    content = models.CharField(max_length=280)
+    publisher = models.CharField(max_length=30)
+    date = models.DateTimeField()
+
+
+
 class FavoriteGame(SafeDeleteModel):
     """
     As we don't have any game model that's why
