0278c37eb1a4b36c68c31ae317ae03ad42716426
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-05-12 17:10:41 +0300

    Add views for platforms and genres

--- gameMuster/api_urls.py
+++ gameMuster/api_urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
-from gameMuster.api import views
+from gameMuster.api.views import GameViewSet, GameDetail, PlatformViewSet, PlatformDetail, GenreViewSet, GenreDetail
 
 router = routers.DefaultRouter()
-router.register(r'games', views.GameViewSet)
+router.register(r'games', GameViewSet)
+router.register(r'platforms', PlatformViewSet)
+router.register(r'genres', GenreViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('games/<int:pk>', GameDetail.as_view(), name='user-detail'),
+    path('platforms/<int:pk>', PlatformDetail.as_view(), name='platform-detail'),
+    path('genres/<int:pk>', GenreDetail.as_view(), name='genres-detail'),
 ]
