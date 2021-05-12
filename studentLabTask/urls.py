78cbdd7ad39ee33fe174f7dc0cd68281c87cb335
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-05-12 17:20:34 +0300

    Move api urls to api folder

--- studentLabTask/urls.py
+++ studentLabTask/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('gameMuster.urls')),
-    path('', include('gameMuster.api_urls')),
+    path('', include('gameMuster.api.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
