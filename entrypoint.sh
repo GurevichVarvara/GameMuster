c028f9561c64ca4e0fd4c38ef64312915129c1a4
varvarahurevich <gurevichvarvara@gmail.com>
2021-05-21 15:06:51 +0300

    Change docker compose file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python3 manage.py migrate
 python3 manage.py collectstatic
 
-exec gunicorn studentLabTask.wsgi:application --bind 0.0.0.0:8000 --reload
+exec gunicorn studentLabTask.wsgi:application
