8921fd5cabccceac431cfda2bc82e74d4c8e555e
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-05-31 16:08:46 +0300

    Refactore tests

--- gameMuster/tests/test_views.py
+++ gameMuster/tests/test_views.py
@@ -1,24 +1,23 @@
+"""View tests"""
 from django.test import SimpleTestCase, Client
 from django.urls import reverse
-from gameMuster.temp_models import ModelManager
 
 
 class TestViews(SimpleTestCase):
-    """Tests for views
-
-    Tests if urls call proper functions.
-    """
+    """Test if urls call proper functions"""
 
     def setUp(self):
         self.client = Client()
 
-    def test_index_GET(self):
+    def test_index_get(self):
+        """Index view test"""
         response = self.client.get(reverse('index'))
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'gameMuster/index.html')
 
-    def test_detail_GET(self):
+    def test_detail_get(self):
+        """Detail view test"""
         response = self.client.get(reverse('detail', args=(1,)))
 
         self.assertEquals(response.status_code, 200)
